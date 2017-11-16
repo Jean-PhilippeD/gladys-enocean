60ebf83554249cab9b1180c9016a9f12b88c88f4
Jean-PhilippeD <jp.danree@free.fr>
2017-11-16 17:12:22 +0100

    add forget

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ module.exports = function (sails) {
     var connect = require('./lib/enocean.connect.js');
     var setup = require('./lib/enocean.setup.js');
     var learn = require('./lib/enocean.learn.js');
-    var exec = require('./lib/enocean.exec.js');
-
+    var exec = require('./lib/enocean.exec.js');    
+    var forget = require('./lib/enocean.forget.js');
+    
     gladys.on('ready', function(){
         connect();
     });
@@ -15,6 +16,7 @@ module.exports = function (sails) {
         connect,
         setup,
         learn,
-        exec
+        exec,
+        forget
     };
 };
